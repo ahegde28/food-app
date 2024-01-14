5819018c4cd90eddc3cff77559bae4b98c200a74
Abhishek Hegde <ahegde2021@gmail.com>
2024-01-14 18:56:07 +0530

    added react svg, feather icons

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,11 +1,18 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 export default function HomeScreen() {
-    return (
-        <SafeAreaView>
-            <Text>HomeScreen</Text>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView className="bg-white ">
+      <StatusBar barStyle="dark-content" />
+
+      <View className="flex-row items-center space-x-2 px-4 pb-2">
+        <View className="flex-1 flex-row items-center rounded-full border border-gray-300 p-3">
+          <Text>HomeScreen</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
